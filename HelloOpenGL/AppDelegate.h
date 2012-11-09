4292a5c52bed7d16bda653b0cdb693be8feee8b3
Sandro Martis <fennek.rvb@gmx.ch>
2012-11-09 19:18:16 +0100

    Initial Commit

---
+++ HelloOpenGL/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloOpenGL
+//
+//  Created by Sandro Martis on 09.11.12.
+//  Copyright (c) 2012 Sandro Martis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloOpenGL/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloOpenGL
+//
+//  Created by Sandro Martis on 09.11.12.
+//  Copyright (c) 2012 Sandro Martis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
